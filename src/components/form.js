b2266326773dab550910c30fb65ef37494a45740
Ezekiel Mangalu <emangalu@gmail.com>
2017-08-25 17:42:05 +0800

    add interaction on form

--- src/components/form.js
+++ src/components/form.js
@@ -1,18 +1,46 @@
 import React from 'react';
 import './form.css';
 
-export default () => 
-  <form className="form">
-    <label>
-      Book
-      &nbsp;
-      <input type="number" min="1" required/>
-      &nbsp;
-      seat(s) for RM230
-    </label>
+export default class Form extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      price: 230,
+      total: 230,
+      totalSeats: 10,
+      remaining: 10,
+      seats: 0
+    }
 
-    <div className="form-submit">
-      <button type="submit">Book now</button>
-      <small>4 seats remaining</small>
-    </div>
-  </form>
+    this.updateSeats = this.updateSeats.bind(this)
+  }
+
+  updateSeats(e) {
+    let selected = e.target.value
+    let { seats, price, totalSeats, remaining } = this.state
+    this.setState({seats: selected})
+    this.setState({total: price * seats})
+    this.setState({remaining: totalSeats - selected })
+  }
+
+  render() {
+    let { total, totalSeats, remaining, seats } = this.state
+
+    return (
+      <form className="form">
+        <label>
+          Book
+          &nbsp;
+          <input type="number" min="1" max={ totalSeats } value={ seats } onChange={ this.updateSeats } required/>
+          &nbsp;
+          seat(s) for RM{ total }
+        </label>
+
+        <div className="form-submit">
+          <button type="submit">Book now</button>
+          <small>{ remaining } seats remaining</small>
+        </div>
+      </form>
+    )
+  }
+}
